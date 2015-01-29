b8095795e46a4199c39a3560c188b0232f771f81
Koichiro Sumi <koichiro.sumi@actcat.co.jp>
2015-01-29 10:42:41 +0900

    add note model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716065735) do
+ActiveRecord::Schema.define(version: 20150129014147) do
 
   create_table "bananas", force: true do |t|
     t.string   "name"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20140716065735) do
     t.string   "name"
     t.integer  "price"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "notes", force: true do |t|
+    t.string   "title"
+    t.string   "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
