3b84767433faa9a15985c78d22ae844f77dd00a8
Haruya Nishikubo <nishikubo@clear-inc.net>
2015-03-04 18:15:51 +0900

    jshint でエラーの出るコードを追記（２回目

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -7,6 +7,7 @@
 
 $("a.github_auth").click(function () {
   ga('send', 'event', 'button', 'click', {'page': '/users/auth/github'})
+  ga('send', 'event', 'button', 'click', {'page': '/users/auth/github'})
 });
 
 mixpanel.track_links("a.github_auth", "Click GitHub Auth");
