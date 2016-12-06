71d50cb12768325c0a39d8e7e3871998c84b4085
Akito Kasai <a.kasai415@gmail.com>
2016-12-06 17:18:40 +0900

    ReekのREADMEにあったsampleを追加

--- app/controllers/bananas_controller.rb
+++ app/controllers/bananas_controller.rb
@@ -3,6 +3,17 @@ class BananasController < ApplicationController
   attr_accessor :banana, :lemon
 
   before_action :set_banana, only: [:show, :edit, :update, :destroy]
+  class Duck
+    class << self
+      def duck_names
+        %i!tick trick track!.each do |surname|
+          %i!duck!.each do |last_name|
+            puts "full name is #{surname} #{last_name}"
+          end
+        end
+      end
+    end
+  end
 
   # GET /bananas
   #
